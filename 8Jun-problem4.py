f2d5448f715608bd9b05f4d805beb2c6310cdadf
Cloud User <ec2-user@ip-172-31-6-184.ap-south-1.compute.internal>
2019-06-10 16:40:46 +0000

    8Jun-problem4.py

---
+++ 8Jun-problem4.py
@@ -0,0 +1,18 @@
+
+import os
+import crypt
+
+n=[]
+for i in range(0,9):
+	n.append(str(i))
+username=input('Enter username you want to add :')
+password='hello'+username
+for i in username:
+	if i in n:
+		print('Cannot create user')
+
+#os.system('sudo useradd  '+username)
+encpass=crypt.crypt(password,'123')
+os.system('sudo useradd -p '+ encpass +' '+username)
+ 
+print("User created")
