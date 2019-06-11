92a79960a7c65a3c5b8bc60692ffdc270c4313ac
ARPIT443 <arpit443@gmail.com>
2019-06-11 17:57:21 +0530

    Create sender.py

---
+++ sender.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2
+
+import  socket 
+recv_ip="127.0.0.1"
+recv_port=4444       #you can check free udp port netstat -nulp
+
+#  creating  udp socket
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+#  sending  data  to target  
+s.sendto("hello",(recv_ip,recv_port)) 
+
