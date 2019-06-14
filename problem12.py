33308a67e4992626833c983295f322e56228cb03
Cloud User <ec2-user@ip-172-31-35-229.ap-south-1.compute.internal>
2019-06-14 18:07:37 +0000

    QR code on web

--- problem12.py
+++ problem12.py
@@ -1,6 +1,7 @@
 import webbrowser
 import pyqrcode
 from googlesearch import search
+import os
 data=input('Enter your search--->>')
 webbrowser.open_new_tab('https://www.google.com/search?q='+data)
 list=[]
@@ -9,7 +10,16 @@ for i in search(data,stop=3):
 j=1
 for i in list:
 	c=pyqrcode.create(i)
-	c.svg("qrcode"+str(j),scale=8)
+	c.svg(str(j)+'.svg',scale=8)
 	j=j+1
+
+file=input('Enter name of .html file :')
+
+os.system('touch'+' '+file)
+os.system('echo "<html><h1>QR code for your search :</h1><body><img src="1.svg"><img src="2.svg"><img src="3.svg"></body></html>">'+file )
+os.system('sudo mv '+file+' '+'/var/www/html/' )
+os.system('sudo mv 1.svg /var/www/html/' )
+os.system('sudo mv 2.svg /var/www/html/' )
+os.system('sudo mv 3.svg /var/www/html/' )
 
 
